d22f7db70c75661ba5fefbe4a57f3f299b0b1672
ltarcher <59073821@qq.com>
2025-12-08 00:49:50 +0800

    将NDK版本固定为27.0.12077973

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.live2d.flutter_live2d_lipsync_example"
     compileSdk = 35
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
